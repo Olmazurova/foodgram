7559eef637a77fb24ccf9e2a41b348c692768850
Olga Mazurova <vsilena@mail.ru>
2025-07-10 18:19:52 +0300

    Написана модель User.

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+# В интерфейс админ-зоны нужно вывести необходимые поля моделей и настроить поиск:
+# вывести все модели с возможностью редактирования и удаления записей;
+# для модели пользователей добавить поиск по адресу электронной почты и имени пользователя;
+
+admin.site.empty_value_display = 'Не задано'
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Администрирование пользователей."""
+
+    list_display = ('first_name', 'last_name', 'username', 'emai', 'password', 'avatar')
+    search_fields = ('email', 'first_name')
