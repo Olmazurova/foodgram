3b01f87667f98dd4a4d0bd26b26b5b27d3bf6d54
Olga Mazurova <vsilena@mail.ru>
2025-07-22 16:52:42 +0300

    Поправлена админка рецептов.

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -12,10 +12,15 @@ admin.site.register(Subscription)
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'author')
+    list_display = ('name', 'author', 'favorites_count')
     search_fields = ('author', 'name')
     list_filter = ('tags',)
-    # общее число добавлений в избранное
+    readonly_fields = ('favorites_count',)
+
+    def favorites_count(self, obj):
+        return obj.is_favorited.count()
+
+    favorites_count.short_description = 'Количество добавлений в избранное'
 
 
 @admin.register(Ingredient)
