b2953d301f3a70b16db1acfffdd0716a6acef2aa
Olga Mazurova <vsilena@mail.ru>
2025-07-27 18:23:45 +0300

    Добавлены маршруты для статических страниц.

--- backend/api_foodgram/urls.py
+++ backend/api_foodgram/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path, re_path
+from django.views.generic import TemplateView
 
 from api.constants import PREFIX
 from api.views import DecodeLinkView
@@ -12,6 +13,20 @@ urlpatterns = [
         rf'{PREFIX}/(?P<hex_id>(0x)?[0-9a-f]+)/',
         DecodeLinkView.as_view(),
         name='decode_link'
+    ),
+    path(
+        'api/about/',
+        TemplateView.as_view(
+            template_name='frontend/src/pages/about/index.js'
+        ),
+        name='about'
+    ),
+    path(
+        'api/technologies/',
+        TemplateView.as_view(
+            template_name='frontend/src/pages/technologies/index.js'
+        ),
+        name='technologies'
     ),
 ]
 
