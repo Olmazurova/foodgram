5fb65f4785c681d78274ca3393d6544c36fb8aee
Olga Mazurova <vsilena@mail.ru>
2025-07-27 18:43:32 +0300

    Добавлен .env.example.

--- backend/api_foodgram/urls.py
+++ backend/api_foodgram/urls.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path, re_path
-from django.views.generic import TemplateView
 
 from api.constants import PREFIX
 from api.views import DecodeLinkView
@@ -13,20 +12,6 @@ urlpatterns = [
         rf'{PREFIX}/(?P<hex_id>(0x)?[0-9a-f]+)/',
         DecodeLinkView.as_view(),
         name='decode_link'
-    ),
-    path(
-        'api/about/',
-        TemplateView.as_view(
-            template_name='frontend/src/pages/about/index.js'
-        ),
-        name='about'
-    ),
-    path(
-        'api/technologies/',
-        TemplateView.as_view(
-            template_name='frontend/src/pages/technologies/index.js'
-        ),
-        name='technologies'
     ),
 ]
 
