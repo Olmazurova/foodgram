76548ac4a25524ac66cdaf6983a5f4553147be56
Olga Mazurova <vsilena@mail.ru>
2025-07-31 16:10:51 +0300

    Изменения в djoser permissions.

--- backend/api_foodgram/settings.py
+++ backend/api_foodgram/settings.py
@@ -66,12 +66,14 @@ WSGI_APPLICATION = 'api_foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env.str('POSTGRES_DB', 'django'),
-        'USER': env.str('POSTGRES_USER', 'django'),
-        'PASSWORD': env.str('POSTGRES_PASSWORD', ''),
-        'HOST': env.str('DB_HOST', ''),
-        'PORT': env.int('DB_PORT', 5432),
+        # 'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': env.str('POSTGRES_DB', 'django'),
+        # 'USER': env.str('POSTGRES_USER', 'django'),
+        # 'PASSWORD': env.str('POSTGRES_PASSWORD', ''),
+        # 'HOST': env.str('DB_HOST', ''),
+        # 'PORT': env.int('DB_PORT', 5432),
     }
 }
 
@@ -137,7 +139,7 @@ DJOSER = {
     },
     'PERMISSIONS': {
         'user_list': ['rest_framework.permissions.AllowAny'],
-        'user': ['djoser.permissions.CurrentUserOrAdmin'],
+        'user': ['rest_framework.permissions.IsAuthenticatedOrReadOnly'],
     },
 }
 
